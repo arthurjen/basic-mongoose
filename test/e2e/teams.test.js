0f85b4a7dd3256288d16cd2c2ab8f056a92d317a
Arthur Jen <arthurljen@gmail.com>
2018-07-17 18:00:37 -0700

    all e2e scaffolding complete and ready

---
+++ test/e2e/teams.test.js
@@ -0,0 +1,9 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+describe('Teams API', () => {
+    it('works', () => {
+        assert.isOk(true);
+    });
+});
