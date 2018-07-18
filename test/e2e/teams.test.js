9427bf7e812c77bfe9bb500dfa96c930152f9eb8
Arthur Jen <arthurljen@gmail.com>
2018-07-17 18:05:02 -0700

    completed POST method

--- test/e2e/teams.test.js
+++ test/e2e/teams.test.js
@@ -3,7 +3,26 @@ const request = require('./request');
 const { dropCollection } = require('./db');
 
 describe('Teams API', () => {
-    it('works', () => {
-        assert.isOk(true);
+    beforeEach(() => dropCollection('teams'));
+
+    let team;
+
+    beforeEach(() => {
+        const data = {
+            name: 'Evil Geniuses',
+            region: 'North America',
+            members: ['Arteezy', 'Suma1l', 's4', 'Cr1t-', 'Fly'],
+            coach: 'BuLba',
+            invited: false
+        };
+        
+        return request
+            .post('/api/teams')
+            .send(data)
+            .then(({ body }) => team = body);
     });
+
+    it('saves a team', () => {
+        assert.isOk(team._id);
+    })
 });
