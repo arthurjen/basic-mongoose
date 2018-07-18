16341027f7a92d7ee92c7efcb8df829444ea0622
Arthur Jen <arthurljen@gmail.com>
2018-07-17 17:11:05 -0700

    all validation tests complete

--- test/unit/teams.test.js
+++ test/unit/teams.test.js
@@ -25,4 +25,61 @@ describe('Events model', () => {
         assert.deepEqual(json, data);
         assert.isUndefined(team.validateSync());
     });
+
+    it('validates required fields', () => {
+        const team = new Team({});
+        const errors = getErrors(team.validateSync(), 2);
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.members.kind, 'user defined');
+        assert.equal(errors.members.message, 'there must be 5 members');
+    });
+
+    it('validates region', () => {
+        const team = new Team({
+            name: 'Evil Geniuses',
+            region: 'Africa',
+            members: ['Arteezy', 'Suma1l', 's4', 'Cr1t-', 'Fly']
+        });
+        const errors = getErrors(team.validateSync(), 1);
+        assert.equal(errors.region.kind, 'enum');
+    });
+
+    it('validates member name min length', () => {
+        const team = new Team({
+            name: 'Evil Geniuses',
+            members: ['Arteezy', 'Suma1l', 's', 'Cr1t-', 'Fly']
+        });
+        const errors = getErrors(team.validateSync(), 1);
+        assert.equal(errors['members.2'].kind, 'minlength');
+    });
+
+    it('validates member name max length', () => {
+        const team = new Team({
+            name: 'Evil Geniuses',
+            members: ['Arteezy', 'Suma1234567890123456l', 's4', 'Cr1t-', 'Fly']
+        });
+        const errors = getErrors(team.validateSync(), 1);
+        assert.equal(errors['members.1'].kind, 'maxlength');
+    });
+
+    it('validates coach name max length', () => {
+        const team = new Team({
+            name: 'Evil Geniuses',
+            members: ['Arteezy', 'Suma1l', 's4', 'Cr1t-', 'Fly'],
+            coach: 'BuLbabcdefghijklmnopqrstuvwxyz'
+
+        });
+        const errors = getErrors(team.validateSync(), 1);
+        assert.equal(errors.coach.kind, 'maxlength');
+    });
+
+    it('validates invited default is false', () => {
+        const team = new Team({
+            name: 'Evil Geniuses',
+            members: ['Arteezy', 'Suma1l', 's4', 'Cr1t-', 'Fly']
+        });
+        const json = team.toJSON();
+        assert.strictEqual(json.invited, false);
+    });
+
 });
